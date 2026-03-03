45be2f0996ea4ce24741e01122f11d686238bc87
Md Noornawaz Rahman <noornawazrahman@gmail.com>
2026-03-03 13:40:23 +0000

    XIX

---
+++ 2026-03-03/contests/Codeforces/NeowiseLabsContest1(CodeforcesRound1018,Div1Div2)/AWonderfulSticks/solution.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define print(v)                               \
+    ([&]() {                                   \
+        int n = v.size();                      \
+        for (int i = 0; i < n; i++) {          \
+            cout << v[i] << " \n"[i == n - 1]; \
+        }                                      \
+    }())
+#define scan(n)                                  \
+    ([&]() {                                     \
+        vi v(n);                                 \
+        for (int i = 0; i < n; i++) cin >> v[i]; \
+        return v;                                \
+    }())
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+const char nl = '\n';
+const char sp = ' ';
+inline void yes() { cout << "YES" << nl; }
+inline void no() { cout << "NO" << nl; }
+void solve();
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int mini = 1;
+    int maxi = n;
+    vi ans;
+    for (int i = n - 2; i >= 0; i--) {
+        if (s[i] == '<') {
+            ans.push_back(mini++);
+        } else {
+            ans.push_back(maxi--);
+        }
+    }
+    ans.push_back(maxi);
+    reverse(all(ans));
+    print(ans);
+}
