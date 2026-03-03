0772ab9aac2fcaa5dea19da47b99dfdd0e2cf474
Md Noornawaz Rahman <noornawazrahman@gmail.com>
2026-03-03 03:52:20 +0000

    XVII

---
+++ 2026-03-03/contests/Codeforces/CodeforcesRound1024(Div2)/ADinnerTime/solution.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define print(v)                               \
+    ([&]() {                                   \
+        int n = v.size();                      \
+        for (int i = 0; i < n; i++) {          \
+            cout << v[i] << " \n"[i == n - 1]; \
+        }                                      \
+    }())
+#define scan(n)                                  \
+    ([&]() {                                     \
+        vi v(n);                                 \
+        for (int i = 0; i < n; i++) cin >> v[i]; \
+        return v;                                \
+    }())
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+const char nl = '\n';
+const char sp = ' ';
+inline void yes() { cout << "YES" << nl; }
+inline void no() { cout << "NO" << nl; }
+void solve();
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
+
+void solve() {
+    int n, m, p, q;
+    cin >> n >> m >> p >> q;
+    if (n % p == 0) {
+        if (n / p * q == m) {
+            yes();
+        } else {
+            no();
+        }
+    } else {
+        yes();
+    }
+}
