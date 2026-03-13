7998da80483da95f7a2a83bd9fff062c2adb54d1
Md Noornawaz Rahman <noornawazrahman@gmail.com>
2026-03-13 09:22:24 +0000

    XXVII

---
+++ 2026-03-12/contests/Codeforces/CodeforcesRound941(Div2)/ACardExchange/solution.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define print(v)                               \
+    ([&]() {                                   \
+        int n = v.size();                      \
+        for (int i = 0; i < n; i++) {          \
+            cout << v[i] << " \n"[i == n - 1]; \
+        }                                      \
+    }())
+#define scan(n)                                  \
+    ([&]() {                                     \
+        vi v(n);                                 \
+        for (int i = 0; i < n; i++) cin >> v[i]; \
+        return v;                                \
+    }())
+#define ceildiv(a, b) (((a) + (b) - 1) / (b))
+#define yes() cout << "YES" << nl
+#define no() cout << "NO" << nl
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << nl;
+#else
+#define debug(x)
+#endif
+const char nl = '\n';
+const char sp = ' ';
+void solve();
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vi c = scan(n);
+    umap<int, int> freq;
+    for (int i = 0; i < n; i++) {
+        freq[c[i]]++;
+    }
+    for (auto &[key, v] : freq) {
+        if (v >= k) {
+            cout << k - 1 << nl;
+            return;
+        }
+    }
+    cout << n << nl;
+}
