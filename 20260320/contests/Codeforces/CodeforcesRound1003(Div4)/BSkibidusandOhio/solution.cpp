199b3808b4212fde951f6091ac0b971e8e02ff21
Md Noornawaz Rahman <noornawazrahman@gmail.com>
2026-03-21 08:40:08 +0000

    XXXIV

---
+++ 20260320/contests/Codeforces/CodeforcesRound1003(Div4)/BSkibidusandOhio/solution.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define print(v)                               \
+    ([&]() {                                   \
+        int n = v.size();                      \
+        for (int i = 0; i < n; i++) {          \
+            cout << v[i] << " \n"[i == n - 1]; \
+        }                                      \
+    }())
+#define scan(n)                                  \
+    ([&]() {                                     \
+        vi v(n);                                 \
+        for (int i = 0; i < n; i++) cin >> v[i]; \
+        return v;                                \
+    }())
+#define ceildiv(a, b) (((a) + (b) - 1) / (b))
+#define yes() cout << "YES" << nl
+#define no() cout << "NO" << nl
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << nl;
+#else
+#define debug(x)
+#endif
+const char nl = '\n';
+const char sp = ' ';
+void solve();
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
+
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.length();
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] == s[i + 1]) {
+            cout << 1 << nl;
+            return;
+        }
+    }
+    cout << n << nl;
+}
