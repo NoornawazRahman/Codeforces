802df7fd15f6923a63b15e0784e48b9f8dc0ff73
Md Noornawaz Rahman <noornawazrahman@gmail.com>
2026-03-22 02:29:22 +0000

    XXXV

---
+++ 20260321/contests/Codeforces/CodeforcesRound868(Div2)/BSortwithStep/solution.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define print(v)                               \
+    ([&]() {                                   \
+        int n = v.size();                      \
+        for (int i = 0; i < n; i++) {          \
+            cout << v[i] << " \n"[i == n - 1]; \
+        }                                      \
+    }())
+#define scan(n)                                  \
+    ([&]() {                                     \
+        vi v(n);                                 \
+        for (int i = 0; i < n; i++) cin >> v[i]; \
+        return v;                                \
+    }())
+#define ceildiv(a, b) (((a) + (b) - 1) / (b))
+#define yes() cout << "YES" << nl
+#define no() cout << "NO" << nl
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << nl;
+#define vdebug(a)                     \
+    cout << #a << " = ";              \
+    for (auto x : a) cout << x << sp; \
+    cout << nl;
+#else
+#define debug(x)
+#define vdebug(a)
+#endif
+const char nl = '\n';
+const char sp = ' ';
+void solve();
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vi p = scan(n);
+    int c = 0;
+    for (int i = 0; i < n; i++) {
+        if (p[i] % k != (i + 1) % k) {
+            c++;
+        }
+    }
+    int ans;
+    if (c == 0) {
+        ans = 0;
+    } else if (c == 2) {
+        ans = 1;
+    } else {
+        ans = -1;
+    }
+    cout << ans << nl;
+}
---
+++ 20260321/contests/Codeforces/EducationalCodeforcesRound151(RatedforDiv2)/BComeTogether/solution.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define print(v)                               \
+    ([&]() {                                   \
+        int n = v.size();                      \
+        for (int i = 0; i < n; i++) {          \
+            cout << v[i] << " \n"[i == n - 1]; \
+        }                                      \
+    }())
+#define scan(n)                                  \
+    ([&]() {                                     \
+        vi v(n);                                 \
+        for (int i = 0; i < n; i++) cin >> v[i]; \
+        return v;                                \
+    }())
+#define ceildiv(a, b) (((a) + (b) - 1) / (b))
+#define yes() cout << "YES" << nl
+#define no() cout << "NO" << nl
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << nl;
+#define vdebug(a)                     \
+    cout << #a << " = ";              \
+    for (auto x : a) cout << x << sp; \
+    cout << nl;
+#else
+#define debug(x)
+#define vdebug(a)
+#endif
+const char nl = '\n';
+const char sp = ' ';
+void solve();
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
+
+int manhattan_distance(int x1, int y1, int x2, int y2) {
+    return abs(x1 - x2) + abs(y1 - y2);
+}
+
+void solve() {
+    int a1, a2, b1, b2, c1, c2;
+    cin >> a1 >> a2 >> b1 >> b2 >> c1 >> c2;
+    int ans = (manhattan_distance(a1, a2, b1, b2) +
+               manhattan_distance(a1, a2, c1, c2) -
+               manhattan_distance(b1, b2, c1, c2)) /
+                  2 +
+              1;
+    cout << ans << nl;
+}
