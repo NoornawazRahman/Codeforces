180a3a428940935690615455249dbcdad13e73b9
Md Noornawaz Rahman <noornawazrahman@gmail.com>
2026-03-30 04:29:07 +0000

    XL

---
+++ 20260329/contests/Codeforces/EducationalCodeforcesRound130(RatedforDiv2)/BPromo/solution.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define print(v)                               \
+    ([&]() {                                   \
+        int n = v.size();                      \
+        for (int i = 0; i < n; i++) {          \
+            cout << v[i] << " \n"[i == n - 1]; \
+        }                                      \
+    }())
+#define scan(n)                                  \
+    ([&]() {                                     \
+        vi v(n);                                 \
+        for (int i = 1; i < n; i++) cin >> v[i]; \
+        return v;                                \
+    }())
+#define ceildiv(a, b) (((a) + (b) - 1) / (b))
+#define yes() cout << "YES" << nl
+#define no() cout << "NO" << nl
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << nl;
+#define vdebug(a)                     \
+    cerr << #a << " = ";              \
+    for (auto x : a) cerr << x << sp; \
+    cerr << nl;
+#else
+#define debug(x)
+#define vdebug(a)
+#endif
+const char nl = '\n';
+const char sp = ' ';
+void solve();
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int test = 1;
+    /* cin >> test; */
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vi p = scan(n + 1);
+    sort(all(p));
+    vi prefix(n + 1);
+    for (int i = 1; i <= n; i++) {
+        prefix[i] = prefix[i - 1] + p[i];
+    }
+    vdebug(prefix);
+    while (q--) {
+        int x, y;
+        cin >> x >> y;
+        int ans = prefix[n - x + y] - prefix[n - x];
+        cout << ans << nl;
+    }
+}
