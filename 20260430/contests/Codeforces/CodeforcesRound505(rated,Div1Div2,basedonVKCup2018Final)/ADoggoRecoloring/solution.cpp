c8b67cf00a10e898a1d449f06265274e5e23e2a5
Md Noornawaz Rahman <noornawazrahman@gmail.com>
2026-04-30 13:30:14 +0000

    LXX

---
+++ 20260430/contests/Codeforces/CodeforcesRound505(rated,Div1Div2,basedonVKCup2018Final)/ADoggoRecoloring/solution.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define print(v)                               \
+    ([&]() {                                   \
+        int n = v.size();                      \
+        for (int i = 0; i < n; i++) {          \
+            cout << v[i] << " \n"[i == n - 1]; \
+        }                                      \
+    }())
+#define scan(n)                                  \
+    ([&]() {                                     \
+        vector<int> v(n);                        \
+        for (int i = 0; i < n; i++) cin >> v[i]; \
+        return v;                                \
+    }())
+#define ceildiv(a, b) (((a) + (b) - 1) / (b))
+#define yes() cout << "Yes" << nl
+#define no() cout << "No" << nl
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << nl;
+#define vdebug(a)                     \
+    cerr << #a << " = ";              \
+    for (auto x : a) cerr << x << sp; \
+    cerr << nl;
+#else
+#define debug(x)
+#define vdebug(a)
+#endif
+const char nl = '\n';
+const char sp = ' ';
+void solve();
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int test = 1;
+    // cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
+
+void solve() {
+    int n;
+    string s;
+    vector<int> freq(128);
+    cin >> n >> s;
+    if (n == 1) {
+        yes();
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        freq[s[i]]++;
+    }
+    for (int i = 'a'; i <= 'z'; i++) {
+        if (freq[i] >= 2) {
+            yes();
+            return;
+        }
+    }
+    no();
+}
