c928d8197990a02833a9bc6973b3e60189c7cc4b
Md Noornawaz Rahman <noornawazrahman@gmail.com>
2026-09-02 21:45:41 +0530

    XCVI

---
+++ 20260801/C_Red_Black_Pairs.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define print(v)                               \
+    ([&]() {                                   \
+        int n = v.size();                      \
+        for (int i = 0; i < n; i++) {          \
+            cout << v[i] << " \n"[i == n - 1]; \
+        }                                      \
+    }())
+#define scan(n)                     \
+    ([&]() {                        \
+        vi v(n);                    \
+        for (int i = 0; i < n; i++) \
+            cin >> v[i];            \
+        return v;                   \
+    }())
+#define ceildiv(a, b) (((a) + (b) - 1) / (b))
+#define yes() cout << "YES" << nl
+#define no() cout << "NO" << nl
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << nl;
+#define vdebug(a)        \
+    cerr << #a << " = "; \
+    for (auto x : a)     \
+        cerr << x << sp; \
+    cerr << nl;
+#else
+#define debug(x)
+#define vdebug(a)
+#endif
+const char nl = '\n';
+const char sp = ' ';
+void solve();
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
+
+int f(int i, int n, vector<int> &dp, string &a, string &b) {
+    if (i == n) return 0;
+
+    if (i == n - 1) {
+        if (a[i] == b[i]) return 0;
+        return 1;
+    }
+
+    if (dp[i] != -1) return dp[i];
+
+    int ver = f(i + 1, n, dp, a, b);
+    if (a[i] != b[i]) ver++;
+
+    int hor = f(i + 2, n, dp, a, b);
+    if (a[i] != a[i + 1]) hor++;
+    if (b[i] != b[i + 1]) hor++;
+
+    int ans = min(hor, ver);
+    dp[i] = ans;
+    return dp[i];
+}
+
+void solve() {
+    int n;
+    string a, b;
+    cin >> n >> a >> b;
+    vector<int> dp(n + 1, -1);
+    int ans = f(0, n, dp, a, b);
+    cout << ans << nl;
+}
